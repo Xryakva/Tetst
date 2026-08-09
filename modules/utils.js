88bfb313823eed99e52abfc89feb2a65a258e003
Xryakva <matvey.bizbess@gmail.com>
2026-08-09 22:17:11 +0300

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,1 +1,6 @@
-const fs = require('fs'); const sleep = ms => new Promise(r => setTimeout(r, ms)); const readLines = file => fs.readFileSync(file, 'utf8').split('\n').filter(l => l.trim()); const writeIndex = file => { const idx = parseInt(fs.readFileSync(file, 'utf8')) || 0; fs.writeFileSync(file, String(idx + 1)); return idx + 1; }; const getIndex = file => parseInt(fs.readFileSync(file, 'utf8')) || 0; module.exports = { sleep, readLines, writeIndex, getIndex };
+const fs = require('fs');
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+const readLines = file => { try { return fs.readFileSync(file, 'utf8').split('\n').filter(l => l.trim()); } catch(e) { return []; } };
+const writeIndex = file => { const idx = parseInt(fs.readFileSync(file, 'utf8')) || 0; fs.writeFileSync(file, String(idx + 1)); return idx + 1; };
+const getIndex = file => { try { return parseInt(fs.readFileSync(file, 'utf8')) || 0; } catch(e) { fs.writeFileSync(file, '1'); return 1; } };
+module.exports = { sleep, readLines, writeIndex, getIndex };
